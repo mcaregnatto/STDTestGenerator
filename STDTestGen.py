3f913f1cb79c5a3992507cd617582fae85a5db99
mcaregnatto <mcaregnatto@gmail.com>
2023-06-10 17:51:04 -0300

    GUI optimized v2 + alerts

--- STDTestGen.py
+++ STDTestGen.py
@@ -2,24 +2,23 @@ import sys
 import os
 import shutil
 import datetime
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QSpinBox, QFileDialog, QMessageBox 
-from PyQt5.QtCore import Qt, QRegExp, QSize
-from PyQt5.QtGui import QRegExpValidator, QPixmap, QIcon
 import openpyxl
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QSpinBox, QFileDialog, QMessageBox
+from PyQt5.QtCore import Qt, QRegExp, QSize
+from PyQt5.QtGui import QRegExpValidator, QPixmap, QIcon, QFont
 
 class TestRequestApp(QWidget):
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('STD Test Generator')
-        self.setFixedSize(500, 600)
-        
+        self.setFixedSize(500, 650)
+        font = QFont("Segoe UI", 10)
+        QApplication.instance().setFont(font)         
         layout = QVBoxLayout()
         layout.addSpacing(10)
-
 
         # Item 1
         self.sol_input = QLineEdit()
@@ -28,7 +27,6 @@ class TestRequestApp(QWidget):
         validator = QRegExpValidator(regex)
         self.sol_input.setValidator(validator)
         self.sol_input.textChanged.connect(self.insert_slash)
-
         search_button = QPushButton('Buscar')
         search_button.clicked.connect(self.search)
 
@@ -41,12 +39,10 @@ class TestRequestApp(QWidget):
         layout.addLayout(hbox1)
         layout.addSpacing(10)
 
-
         # Item 1.1 - 1.4
-
         self.linha_label = QLabel('Linha:')
         self.linha_input = QLineEdit()
-        self.linha_input.setFixedWidth(430)
+        self.linha_input.setFixedWidth(420)
         self.linha_input.setReadOnly(True)
         self.linha_input.setStyleSheet('background-color: lightgray')
         hbox2 = QHBoxLayout()
@@ -54,10 +50,9 @@ class TestRequestApp(QWidget):
         hbox2.addWidget(self.linha_input)
         layout.addLayout(hbox2)
 
-
         self.familia_label = QLabel('Familia:')
         self.familia_input = QLineEdit()
-        self.familia_input.setFixedWidth(430)
+        self.familia_input.setFixedWidth(420)
         self.familia_input.setReadOnly(True)
         self.familia_input.setStyleSheet('background-color: lightgray')
         hbox3 = QHBoxLayout()
@@ -67,7 +62,7 @@ class TestRequestApp(QWidget):
 
         self.projeto_label = QLabel('Projeto:')
         self.projeto_input = QLineEdit()
-        self.projeto_input.setFixedWidth(430)
+        self.projeto_input.setFixedWidth(420)
         self.projeto_input.setReadOnly(True)
         self.projeto_input.setStyleSheet('background-color: lightgray')
         hbox4 = QHBoxLayout()
@@ -77,14 +72,13 @@ class TestRequestApp(QWidget):
 
         self.modelo_label = QLabel('Modelo:')
         self.modelo_input = QLineEdit()
-        self.modelo_input.setFixedWidth(430)
+        self.modelo_input.setFixedWidth(420)
         self.modelo_input.setReadOnly(True)
         self.modelo_input.setStyleSheet('background-color: lightgray')
         hbox5 = QHBoxLayout()
         hbox5.addWidget(self.modelo_label)
         hbox5.addWidget(self.modelo_input)
         layout.addLayout(hbox5)
-
         layout.addSpacing(20)
 
         # Item 2
@@ -151,15 +145,17 @@ class TestRequestApp(QWidget):
             self.setLayout(layout)
             self.num_samples.valueChanged.connect(self.update_ns_inputs)
             
-
         # layout.addSpacing(500)
         # Item 7
         layout.addSpacing(200)
         create_button = QPushButton('CRIAR DOCUMENTOS DE TESTE  ')
         create_button.clicked.connect(self.create_folders_and_documents)
-        create_button.setStyleSheet('font-weight: bold; height: 35px; width: 210px')
+        create_button.setStyleSheet('font-weight: bold; height: 35px; width: 240px')
         # Set the icon for the button
-        icon_path = 'C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\logo\\plus_doc.png'  # Replace with the path to your icon file
+        #ABAIXO, DIRETÓRIO PC MATEUS
+        icon_path = 'C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\logo\\plus_doc.png'
+        #ABAIXO, DIRETÓRIO PC LAB
+        # icon_path = 'C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\logo\\plus_doc.png'
         create_button.setIcon(QIcon(icon_path))
         create_button.setIconSize(QSize(32, 32))  
         hbox11 = QHBoxLayout()
@@ -168,13 +164,11 @@ class TestRequestApp(QWidget):
         hbox11.addStretch() 
         layout.addLayout(hbox11)
 
-
-
         # Logo
         #ABAIXO, DIRETÓRIO PC MATEUS
         logo = QPixmap('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\logo\\logo.png')
         #ABAIXO, DIRETÓRIO PC LAB
-        # logo = QPixmap('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/01-Templates/logo/logo.png')
+        # logo = QPixmap('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\logo\\logo.png')
         logo = logo.scaled(175, 85, Qt.KeepAspectRatio)
         logo_label = QLabel()
         logo_label.setPixmap(logo)
@@ -194,7 +188,6 @@ class TestRequestApp(QWidget):
         hbox13.addStretch()  
         layout.addLayout(hbox13)
 
-
     def insert_slash(self, text):
         if len(text) == 3 and not text.endswith('/'):
             self.sol_input.setText(text + '/')
@@ -208,7 +201,7 @@ class TestRequestApp(QWidget):
         #ABAIXO, DIRETÓRIO PC MATEUS
         wb = openpyxl.load_workbook('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\00-Solicitações de Teste\\SOLICITAÇÕES DE TESTE.xlsx')
         #ABAIXO, DIRETÓRIO PC LAB
-        # wb = openpyxl.load_workbook('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/00-Solicitações de Teste/SOLICITAÇÕES DE TESTE.xlsx')
+        # wb = openpyxl.load_workbook('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\00-Solicitações de Teste\\SOLICITAÇÕES DE TESTE.xlsx')
         ws = wb.active
 
         found = False
@@ -230,7 +223,7 @@ class TestRequestApp(QWidget):
         #ABAIXO, DIRETÓRIO PC MATEUS
         path = os.path.join('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\main')
         #ABAIXO, DIRETÓRIO PC LAB
-        # path = os.path.join('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/01-Templates/main')
+        # path = os.path.join('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\main')
         for file in os.listdir(path):
             if file.endswith('.xlsx'):
                 self.test_selection.addItem(file[:-5])
@@ -249,7 +242,7 @@ class TestRequestApp(QWidget):
         #ABAIXO, DIRETÓRIO PC MATEUS
         path = os.path.join('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\frf')
         #ABAIXO, DIRETÓRIO PC LAB
-        # path = os.path.join('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/01-Templates/frf')
+        # path = os.path.join('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\frf')
         self.frf_selection.clear()
         self.frf_selection.addItem('')
         for file in os.listdir(path):
@@ -279,11 +272,15 @@ class TestRequestApp(QWidget):
         if not kind_test or not linha or not familia or not projeto or not modelo:
             QMessageBox.warning(self, 'Aviso', 'Preencha todos os campos obrigatórios')
             return
-
+        
+        if num_samples == 0:
+            QMessageBox.warning(self, 'Aviso', 'O número de amostras deve ser maior que zero')
+            return
+        
         #ABAIXO, DIRETÓRIO PC MATEUS
         base_path = 'C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB'
         #ABAIXO, DIRETÓRIO PC LAB
-        # base_path = 'C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB'
+        # base_path = 'C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB'
         
         # Excption if "sonora"
         if "sonora" in kind_test:
@@ -296,7 +293,6 @@ class TestRequestApp(QWidget):
         # Adicionando a pasta vazia "sol_traco" + "-" + "Dados de teste"
         test_data_folder = os.path.join(path, f"{sol_traco} - Dados de teste")
         os.makedirs(test_data_folder, exist_ok=True)
-
         today = datetime.datetime.now().strftime('%Y%m%d')
 
         for i in range(num_samples):
@@ -310,12 +306,11 @@ class TestRequestApp(QWidget):
                 return
             else:
                 entered_serial_numbers.append(ns)
-
             
             #ABAIXO, DIRETÓRIO PC MATEUS
             src_file = os.path.join('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\main', f'{self.test_selection.currentText()}.xlsx')
             #ABAIXO, DIRETÓRIO PC LAB
-            # src_file = os.path.join('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/01-Templates/main', f'{self.test_selection.currentText()}.xlsx')
+            # src_file = os.path.join('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\main', f'{self.test_selection.currentText()}.xlsx')
             dst_file = os.path.join(path, f'{today}_{sol_traco}_{modelo}_{ns}_{kind_test}.xlsx')
             shutil.copy(src_file, dst_file)
 
@@ -323,22 +318,19 @@ class TestRequestApp(QWidget):
                 #ABAIXO, DIRETÓRIO PC MATEUS
                 src_file = os.path.join('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\frf', f'{self.frf_selection.currentText()}.xlsx')
                 #ABAIXO, DIRETÓRIO PC LAB
-                # src_file = os.path.join('C:/Users/ulabeng/Midea Carrier/Engenharia de Produto - LAB/01-Templates/frf', f'{self.frf_selection.currentText()}.xlsx')
-                dst_file_frf = os.path.join(path, f'{today}_{sol_traco}_{modelo}_{ns}_{kind_test}_FRF.xlsx')  # Altere o nome do arquivo de destino para o arquivo FRF
+                # src_file = os.path.join('C:\\Users\\ulabeng\\Midea Carrier\\Engenharia de Produto - LAB\\01-Templates\\frf', f'{self.frf_selection.currentText()}.xlsx')
+                dst_file_frf = os.path.join(path, f'{today}_{sol_traco}_{modelo}_{ns}_{kind_test}_FRF.xlsx') 
                 shutil.copy(src_file, dst_file_frf)  # Copie o arquivo FRF para o novo arquivo de destino
-
 
         # Create shortcut
         shortcut_path = os.path.join(os.path.expanduser('~'), 'Desktop', f'{kind_test}_{sol_traco}_{linha}_{familia}_{projeto}_{modelo}.lnk')
         target_path = os.path.abspath(path)
         self.create_shortcut(shortcut_path, target_path)
-
         QMessageBox.information(self, 'Aviso', 'Concluído com sucesso!')
 
     def create_shortcut(self, shortcut_path, target_path):
         import pythoncom
         from win32com.shell import shell
-
         shortcut = pythoncom.CoCreateInstance(
             shell.CLSID_ShellLink,
             None,
@@ -349,12 +341,14 @@ class TestRequestApp(QWidget):
         shortcut.SetPath(target_path)
         shortcut.SetDescription(f'Shortcut to {target_path}')
         shortcut.SetIconLocation(target_path, 0)
-
         persist_file = shortcut.QueryInterface(pythoncom.IID_IPersistFile)
         persist_file.Save(shortcut_path, 0)
-
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # Define o nome do aplicativo
+    app.setApplicationDisplayName('STD Test Generator')
     ex = TestRequestApp()
     ex.show()
+    ex.setWindowIcon(QIcon('C:\\Users\\Mateus\\Documents\\Projetos\\STD Test Gen\\Engenharia de Produto - LAB\\01-Templates\\ico16.ico'))
     sys.exit(app.exec_())
